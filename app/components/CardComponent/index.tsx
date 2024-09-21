bfbfe313e6422e9937194a3685da3431ee425184
Thayano davalo <ttdavalo@gmail.com>
2024-09-21 19:24:07 -0300

    fix: removendo paramentro icon do cardComponent

--- app/components/CardComponent/index.tsx
+++ app/components/CardComponent/index.tsx
@@ -5,7 +5,7 @@ interface CardContentProps {
 }
 import { FaBeer } from 'react-icons/fa'
 
-export const CardComponent : React.FC<CardContentProps> = ({title, content, icon}) => {
+export const CardComponent : React.FC<CardContentProps> = ({title, content}) => {
     return (
         <div className="bg-white w-72 rounded-xl text-center flex flex-col justify-center items-center gap-2 px-8 py-8">
             <FaBeer color='#343242' size={20}/>
